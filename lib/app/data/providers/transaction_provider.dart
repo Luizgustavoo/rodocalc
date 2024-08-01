3cf0bf2be646d58327c53d8659370341509e4d17
dineifernandes <dinei_aparecido@hotmail.com>
2024-07-31 22:39:30 -0300

    create transactions

--- lib/app/data/providers/transaction_provider.dart
+++ lib/app/data/providers/transaction_provider.dart
@@ -75,7 +75,12 @@ class TransactionApiClient {
         "status": transacoes.status.toString(),
         "pessoa_id": transacoes.pessoaId.toString(),
         "veiculo_id": transacoes.veiculoId.toString(),
-        "data": transacoes.data.toString()
+        "data": transacoes.data.toString(),
+        "origem": transacoes.origem.toString(),
+        "destino": transacoes.destino.toString(),
+        "quantidade_tonelada": transacoes.quantidadeTonelada.toString(),
+        "tipocarga_id": transacoes.tipoCargaId.toString(),
+        "tipo_transacao": transacoes.tipoTransacao.toString(),
       });
 
       request.headers.addAll({
