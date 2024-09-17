6b0008d6382a3be69f7d94c09ccb776bea7e3bca
dineifernandes <dinei_aparecido@hotmail.com>
2024-09-17 13:59:29 -0300

    teste

--- lib/app/data/controllers/transaction_controller.dart
+++ lib/app/data/controllers/transaction_controller.dart
@@ -540,8 +540,9 @@ class TransactionController extends GetxController {
 
     txtValueController.text =
         FormattedInputers.formatValuePTBR(selected.valor!.toString());
-    txtCityController.text =
-        selected.cidade != null ? selected.cidade.toString() : "";
+    txtCityController.text = selected.cidade != null
+        ? "${selected.cidade.toString()}-${selected.uf.toString()}"
+        : "";
     txtCompanyController.text =
         selected.empresa != null ? selected.empresa.toString() : "";
     txtDDDController.text = selected.ddd != null ? selected.ddd.toString() : "";
