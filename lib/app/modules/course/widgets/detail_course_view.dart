4dbf6337c1276269331e4ecd80e95572929a385e
Claudinei Fernandes <dinei_aparecido@hotmail.com>
2024-10-28 14:37:58 -0300

    Final Ios V

--- lib/app/modules/course/widgets/detail_course_view.dart
+++ lib/app/modules/course/widgets/detail_course_view.dart
@@ -3,16 +3,17 @@ import 'package:get/get.dart';
 import 'package:rodocalc/app/global/custom_app_bar.dart';
 import 'package:rodocalc/app/utils/custom_elevated_button.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class DetailCourseView extends StatelessWidget {
-  const DetailCourseView(
-      {super.key,
-      required this.titulo,
-      required this.descricao,
-      required this.linkVideo,
-      required this.imagem,
-      required this.link});
+  const DetailCourseView({
+    super.key,
+    required this.titulo,
+    required this.descricao,
+    required this.linkVideo,
+    required this.imagem,
+    required this.link,
+  });
 
   final String titulo;
   final String descricao;
@@ -22,47 +23,47 @@ class DetailCourseView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final videoId = YoutubePlayer.convertUrlToId(linkVideo);
-    final controller = YoutubePlayerController(
-        initialVideoId: videoId!,
-        flags: const YoutubePlayerFlags(
-          autoPlay: true,
+    // Extraindo o ID do vídeo do link do vídeo
+    final videoId = YoutubePlayerController.convertUrlToId(linkVideo);
+    final controller = YoutubePlayerController.fromVideoId(
+        videoId: videoId!,
+        autoPlay: true,
+        params: const YoutubePlayerParams(
+          showVideoAnnotations: false,
+          strictRelatedVideos: false,
+          enableCaption: false,
           mute: false,
-          showLiveFullscreenButton: true,
         ));
+
     return Scaffold(
       appBar: CustomAppBar(title: titulo.toUpperCase()),
       body: SingleChildScrollView(
-        physics: const NeverScrollableScrollPhysics(),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Hero(tag: 'course_image_$titulo', child: Container()),
             const SizedBox(height: 10),
-            YoutubePlayer(
-              controller: controller,
-              showVideoProgressIndicator: true,
-            ),
+            YoutubePlayer(controller: controller),
             const SizedBox(height: 15),
             CustomElevatedButton(
-                onPressed: () async {
-                  if (linkVideo.isEmpty) {
-                    Get.snackbar(
-                      'Atenção',
-                      'Link do curso não disponível!',
-                      backgroundColor: Colors.orange,
-                      colorText: Colors.white,
-                      snackPosition: SnackPosition.BOTTOM,
-                    );
-                  } else {
-                    await launchUrl(Uri.parse(link));
-                  }
-                },
-                child: const Text(
-                  'LINK PARA COMPRA',
-                  style:
-                      TextStyle(color: Colors.white, fontFamily: 'Inter-Bold'),
-                )),
+              onPressed: () async {
+                if (link.isEmpty) {
+                  Get.snackbar(
+                    'Atenção',
+                    'Link do curso não disponível!',
+                    backgroundColor: Colors.orange,
+                    colorText: Colors.white,
+                    snackPosition: SnackPosition.BOTTOM,
+                  );
+                } else {
+                  await launchUrl(Uri.parse(link));
+                }
+              },
+              child: const Text(
+                'LINK PARA COMPRA',
+                style: TextStyle(color: Colors.white, fontFamily: 'Inter-Bold'),
+              ),
+            ),
             const SizedBox(height: 5),
             Padding(
               padding: const EdgeInsets.all(8.0),
@@ -76,29 +77,22 @@ class DetailCourseView extends StatelessWidget {
                         'DESCRIÇÃO DO CURSO:',
                         style: TextStyle(fontFamily: 'Inter-Black'),
                       ),
-                      const Divider(
-                        color: Colors.orange,
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
+                      const Divider(color: Colors.orange),
+                      const SizedBox(height: 10),
                       SizedBox(
                         height: Get.height * .34,
                         child: ListView(
                           physics: const AlwaysScrollableScrollPhysics(),
-                          shrinkWrap: true,
                           children: [
                             Text(
                               descricao,
                               textAlign: TextAlign.justify,
                               style: const TextStyle(fontFamily: 'Inter-Bold'),
                             ),
-                            const SizedBox(
-                              height: 15,
-                            ),
+                            const SizedBox(height: 15),
                           ],
                         ),
-                      )
+                      ),
                     ],
                   ),
                 ),
