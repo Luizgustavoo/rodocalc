11e4e962f0ae2bf92f2a481a6ce282cc476baabe
Claudinei Fernandes <dinei_aparecido@hotmail.com>
2024-11-01 15:59:33 -0300

    Final V

--- lib/app/modules/global/bottom_navigation.dart
+++ lib/app/modules/global/bottom_navigation.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rodocalc/app/data/controllers/plan_controller.dart';
 import 'package:rodocalc/app/routes/app_routes.dart';
+import 'package:rodocalc/app/utils/service_storage.dart';
 
 class WidgetPlan extends StatelessWidget {
   const WidgetPlan(
@@ -46,32 +47,35 @@ class WidgetPlan extends StatelessWidget {
               ),
             ],
           ),
-          ElevatedButton(
-            onPressed: () async {
-              await planController.getAll();
-              await planController.getMyPlans();
-              if (planController.myPlans.isEmpty) {
-                Get.toNamed(Routes.plan);
-              } else {
-                Get.toNamed(Routes.newplanview);
-              }
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.white,
-              foregroundColor: Colors.red,
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-            child: const Text(
-              'VER PLANOS',
-              style: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
+          ServiceStorage.getUserTypeId() == 4
+              ? SizedBox.shrink()
+              : ElevatedButton(
+                  onPressed: () async {
+                    await planController.getAll();
+                    await planController.getMyPlans();
+                    if (planController.myPlans.isEmpty) {
+                      Get.toNamed(Routes.plan);
+                    } else {
+                      Get.toNamed(Routes.newplanview);
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.red,
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: const Text(
+                    'VER PLANOS',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
         ],
       ),
     );
